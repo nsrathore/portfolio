f2df5b4bcb8fbda93345f9bc94242acf3e56c549
Nikhilendra Rathore <nikhilendra7@gmail.com>
2026-05-02 00:11:39 -0500

    Disable impact section for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <ResumeDownload />
-      <Impact />
+      {/* <Impact /> */}
       <Projects />
       <Skills />
       <About />
